1bde3d19b8a87601dc1f87869d3c1255e73f4cc1
Zak <zhorton999@gmail.com>
2019-02-13 22:46:19 -0600

    Added number prototype methods

--- src/number.js
+++ src/number.js
@@ -1,9 +1,58 @@
 
 /**
  * @param Number 
- * @return bool
+ * @return Bool
  */
- 
 Number.prototype.is = function(number) {
-	return (this === number)
+	return (this.valueOf() === number)
+}
+
+/**
+ * @param Array
+ * @return Bool
+ */
+Number.prototype.isFirstIn = function(array = []) {
+	return (0 === this.valueOf())
+}
+
+/**
+ * @param Array
+ * @return Bool
+ */
+Number.prototype.isItemOneIn = function(array = []) {
+  return (1 === this.valueOf())
+}
+
+/**
+ * @param Array
+ * @return Bool
+ */
+Number.prototype.isLastIn = function(array) {
+
+	return (array.length === this.valueOf())
+}
+
+/**
+ * @param Array
+ * @return Bool
+ */
+Number.prototype.matchesAll = function(array) {
+		let matches = true 
+
+		array.forEach((num) => 
+		{
+			if(!matches) return 
+
+			matches = (this.valueOf() === num)	
+		})
+
+		return matches 
+}
+
+/**
+ * @param Array
+ * @return Bool
+ */
+Number.prototype.matchesAny = function(array) {
+	 return array.includes(this.valueOf())
 }
